f4f7c42ac593a521d92b7fc878f7a1a4c8e804ae
pablo rodriguez <pablors0598@gmail.com>
2021-09-08 16:15:20 -0400

    se implemento un metodo con transaccion para crear una otb y asisgnarle el ipd e la otb creada al usuario que la creo

--- app/Models/OtbsModel.php
+++ app/Models/OtbsModel.php
@@ -19,6 +19,20 @@ class OtbsModel extends Model
             ->where(['Otb_ID'=>$id]) 
             ->first();
     }
+  public function InsertOtb($userId,$data): bool{
+    $userModel=new UsersModel();
+      
+      $this->db->transStart(); //iniciamos la transaccion 
+      $otbId=$this->insert($data); //primera consulta
+      $userModel->update($userId,["Otb_ID"=>$otbId]); //segunda consulta
+      $this->db->transComplete(); // terminando la transaccion
+
+      if($this->db->transStatus()===false){ //verificando el estado de la transaccion
+        return false;
+      }
+      return true;
+
+  }
 }
 
 ?>
